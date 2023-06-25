2127954a6f528e96161eaeca5a5baa1e6cd5222d
Denis <allmediasale@icloud.com>
2023-06-26 01:37:17 +0700

    add function welcome_user with prompt

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -1,13 +1,4 @@
+import prompt
+
 def welcome_user():
-    name = ''
-    while name == '':
-        print('May I have your name? ', end='')
-        name = input()
-
-
-# import prompt
-
-# def welcome_user():
-#    name = prompt.string('May I have your name? ')
-
-# welcome_user()
+   name = prompt.string('May I have your name? ')
