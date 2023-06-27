b3e64cd6645b524e4791c6b05fbd6bbfb33cff7d
Denis <allmediasale@icloud.com>
2023-06-27 13:00:15 +0700

    add flake8

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -1,13 +1,5 @@
-
-# def welcome_user():
-#     name = ''
-#     while name == '':
-#         print('May I have your name? ', end='')
-#         name = input()
-#     if name:
-#         print(f'Hello, {name}!')
-
 import prompt
+
 
 def welcome_user():
    name = prompt.string('May I have your name? ')
