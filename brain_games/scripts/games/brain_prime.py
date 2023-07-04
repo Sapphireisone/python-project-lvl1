2f671ebd8d967f1d1eec87577f119bc41c72145f
Denis <allmediasale@icloud.com>
2023-07-04 10:20:04 +0200

    change brain_prime.py

--- brain_games/scripts/games/brain_prime.py
+++ brain_games/scripts/games/brain_prime.py
@@ -6,27 +6,31 @@ import random
 def find_prime():
     num = random.randint(1, 99)
     print(f'Question: {num}')
-    i = 2
-    res = 0
     result = ''
-    while i < num:
-        if num % i != 0:
-            i += 1
-        else:
-            res += 1
-            break
-    if res > 0:
+    if num == 1:
         result = 'no'
-    else:
+    elif num == 2:
         result = 'yes'
+    else:
+        i = 2
+        res = 0
+        while i < num:
+            if num % i != 0:
+                i += 1
+            else:
+                res += 1
+        if res > 0:
+            result = 'no'
+        else:
+            result = 'yes'
     return result
 
 
 def main():
     hello_user()
     name = welcome_user()
-    print('What is the result of the expression?')
-    i = 3
+    print('Answer "yes" if given number is prime. Otherwise answer "no".')
+    i = 30
     while i > 0:
         result = find_prime()
         answer = input('You answer: ')
