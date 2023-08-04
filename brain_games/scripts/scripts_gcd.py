4514c6f576dde1ed076cfc428cb805f26fe564c9
Denis <allmediasale@icloud.com>
2023-08-04 20:53:27 +0700

    change scripts_gcd.py

--- brain_games/scripts/scripts_gcd.py
+++ brain_games/scripts/scripts_gcd.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 from brain_games.engine import run_game
-from brain_games.scripts.games.brain_gsd import gcd, qwest_gcd
+from brain_games.scripts.games.brain_gcd import gcd, qwest_gcd
 
 
 def main():
